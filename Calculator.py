4b44c5ec1e4d359527dfb522e4724b8603ef57d7
Krithik-Lashwin <125042637+Krithik-Lashwin@users.noreply.github.com>
2023-02-10 18:13:15 +0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+print('Simple Calculator')
+x = 10
+y = 20
+sum = x+y
+product = x*y
+difference = y - x
+quotient = y/x
+remainder = y%x
+print('The sum of two numbers is;', sum)
+print('The product of the two numbers is;', product)
+print('The difference of the two numbers is;', difference)
+print('The quotient of the two numbers is;', quotient)
+print('The remainder of the two numbers is;', remainder)
